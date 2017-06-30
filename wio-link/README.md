3cb370db7d50040d4722a0ff727334bbecca5081
gomesuit <gomesuit@gmail.com>
2017-07-01 00:59:28 +0900

    save

---
+++ wio-link/README.md
@@ -0,0 +1,4 @@
+```sh
+export ACCESS_TOKEN=26d4c3420dde1e5879a1e73beb0d561f
+./sample.sh
+```
---
+++ wio-link/sample.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -xe
+
+curl -sS "https://us.wio.seeed.io/v1/node/GroveTempHumD0/temperature?access_token=$ACCESS_TOKEN"
