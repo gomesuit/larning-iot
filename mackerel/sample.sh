09c83c86a4aaf7f4cd8053796220e56f0f8b3ed7
gomesuit <gomesuit@gmail.com>
2017-07-01 00:45:10 +0900

    save

---
+++ mackerel/sample.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BODY=$(cat << EOS
+[
+  {
+    "name": "Sample.foo",
+    "time": $(date +%s),
+    "value": 30
+  }
+]
+EOS
+)
+
+curl -X POST https://mackerel.io/api/v0/services/IoT/tsdb \
+  -H "X-Api-Key: $MACKEREL_APIKEY" \
+  -H 'Content-Type: application/json' \
+  -d "$BODY"
